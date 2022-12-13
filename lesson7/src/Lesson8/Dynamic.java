734d7cf7e3933772a7ccb358625c1601b0685b63
Alena Andryiashava <alena.andryiashava@gmail.com>
2022-12-13 10:22:28 +0100

    Dynamic Array

---
+++ lesson7/src/Lesson8/Dynamic.java
@@ -0,0 +1,24 @@
+package Lesson8;
+
+public interface Dynamic {
+    void add(int value);
+
+    void addAt(int index, int value);
+
+    int get(int index);
+
+    void remove();
+
+    void removeAt(int index);
+
+    void set(int index, int value);
+
+    boolean contains(int value);
+
+    int size();
+
+    void clear();
+
+    void shrinkSize();
+}
+
