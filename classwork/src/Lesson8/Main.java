3eecdd12f71c39af6bfb2438fcf26eebc391d546
Alena Andryiashava <alena.andryiashava@gmail.com>
2022-12-13 13:28:23 +0100

    Dynamic Array

--- classwork/src/Lesson8/Main.java
+++ classwork/src/Lesson8/Main.java
@@ -45,8 +45,8 @@ public class Main {
         d.print();
 
         System.out.println(d.get(5));
-        System.out.println(d.get(20));
         System.out.println(d.get(6));
+
 
         d.shrinkSize();
         d.print();
