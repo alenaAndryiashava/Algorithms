f9c365c81dda94a2d397930e45e1a5ad5ad39ff6
Alena Andryiashava <alena.andryiashava@gmail.com>
2023-03-07 12:58:15 +0100

    int to Roman

--- Lesson_03_2023/src/CalcSystem/CalcSystem.java
+++ Lesson_03_2023/src/CalcSystem/CalcSystem.java
@@ -19,28 +19,42 @@ public class CalcSystem {
 
     public static String getIntRadix(int number, int radix) {
         List<Character> digits = getDigitalTable();
-        if(radix < 2 || radix >=digits.size() || number < 0){
+        if (radix < 2 || radix >= digits.size() || number < 0) {
             System.out.println("Incorrect arguments");
         }
 
         StringBuilder valueStr = new StringBuilder();
-        while (number >0) {
+        while (number > 0) {
             valueStr.insert(0, digits.get(number % radix));
-                number = number / radix;
-            }
+            number = number / radix;
+        }
 
         return valueStr.toString();
     }
-    /*public static String intToRoman(int num) {
-        int[] keys = {1, 4, 5, 9, 10, 40, 50, 90, 100};
 
-     */
+    public static String intToRoman(int num) {
+        int[] keys = {1, 4, 5,  9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+        String[] values = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+        int i = values.length - 1;
 
-
-
+        if (num <= 0) {
+            throw new IllegalArgumentException("You can't enter negative numbers or zero!");
+        }
+        StringBuilder builder = new StringBuilder();
+        while (num > 0) {
+            while (keys[i] > num) {
+                i--;
+            }
+            builder.append(values[i]);
+            num -= keys[i];
+        }
+        return builder.toString();
+    }
 
 
     public static void main(String[] args) {
         System.out.println(getIntRadix(255, 16));
+
+        System.out.println(intToRoman(6));
     }
 }
